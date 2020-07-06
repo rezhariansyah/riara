e66f61dd6863d4a8dc68b11795f80af186a8fb5b
rezhariansyah <rezharatulangi@gmail.com>
2020-07-06 16:03:32 +0800

    container project and about description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -27,40 +27,38 @@ const About = () => {
             </div>
           </div>
           <div className="col-md-5">
-            <div className="container">
-              <div className="about-title">
-                <h1 className="mt-3">WHO WE ARE</h1>
-                <h4 style={{ color: "#17A2BF" }}>Riara Indonesia</h4>
+            <div className="about-title">
+              <h1 className="mt-3">WHO WE ARE</h1>
+              <h4 style={{ color: "#17A2BF" }}>Riara Indonesia</h4>
+            </div>
+
+            <br />
+            <div className="about-description">
+              <p>
+                Established in 2020 in Bira, Indonesia, Riara Indonesia traces
+                its history building quality ships. Riara provides shipbuilding
+                solutions for Phinisi Boad & Fiberglass Boat. Our qualified and
+                experienced in-house design and engineering team, as well as
+                modern production, and quality check. We offer high grade
+                products and services in new shipbuilding, remodeling, and
+                design interior
+              </p>
+              <div className="about-title mt-4">
+                <h4 style={{ color: "#17A2BF" }}>Our Designs</h4>
               </div>
 
               <br />
-              <div className="about-description">
-                <p>
-                  Established in 2020 in Bira, Indonesia, Riara Indonesia traces
-                  its history building quality ships. Riara provides
-                  shipbuilding solutions for Phinisi Boad & Fiberglass Boat. Our
-                  qualified and experienced in-house design and engineering
-                  team, as well as modern production, and quality check. We
-                  offer high grade products and services in new shipbuilding,
-                  remodeling, and design interior
-                </p>
-                <div className="about-title mt-4">
-                  <h4 style={{ color: "#17A2BF" }}>Our Designs</h4>
-                </div>
-
-                <br />
-              </div>
-              <div className="about-description">
-                <p>
-                  Our of box design makes our work different and unique. Making
-                  it easier for the ouwner to market the product. For the sake
-                  of preserving the environtment. we also maintain effient use
-                  of wood in the construction and eco - friendly machinging
-                  equipment for the oceans
-                </p>
-                <div className="about-mobile">
-                  <AboutModal />
-                </div>
+            </div>
+            <div className="about-description">
+              <p>
+                Our of box design makes our work different and unique. Making it
+                easier for the ouwner to market the product. For the sake of
+                preserving the environtment. we also maintain effient use of
+                wood in the construction and eco - friendly machinging equipment
+                for the oceans
+              </p>
+              <div className="about-mobile">
+                <AboutModal />
               </div>
             </div>
           </div>
